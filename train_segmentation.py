5eb39d278e29f738952451911938da9bd08ad211
Gaurav Meena <gauravmeena0708@gmail.com>
2024-10-29 00:29:00 +0530

    test

--- train_segmentation.py
+++ train_segmentation.py
@@ -100,9 +100,12 @@ class LitUnsupervisedSegmenter(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam([{'params': list(self.projection.parameters()), 'lr': self.cfg.lr1},
-                                      {'params': list(self.prediction.parameters()), 'lr': self.cfg.lr2}])
-        return optimizer
+        optimizer = torch.optim.Adam([
+            {'params': list(self.projection.parameters()), 'lr': self.cfg.lr1, 'weight_decay': 1e-5},
+            {'params': list(self.prediction.parameters()), 'lr': self.cfg.lr2, 'weight_decay': 1e-5}
+        ])
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+        return [optimizer], [scheduler]
 
     def on_train_start(self):
         tb_metrics = {
